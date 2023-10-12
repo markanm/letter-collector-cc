0716720da7914cd54f8baf4e5608a03b30832bff
Marko Dzimbeg <marko.dzimbeg@mediavalet.com>
2023-10-13 00:32:36 +0200

    feat: implement path traverse

--- server/LetterCollector.Api/LetterCollector.Api/Services/PathAnalyzerService.cs
+++ server/LetterCollector.Api/LetterCollector.Api/Services/PathAnalyzerService.cs
@@ -1,4 +1,5 @@
 ﻿using LetterCollector.Api.Models;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace LetterCollector.Api.Services
@@ -7,28 +8,45 @@ namespace LetterCollector.Api.Services
     {
         private const char StartingChar = '@';
         private const string EndingCharRegex = "[xX]";
+        private const string ValidLetterRegex = "(?i)[a-wyz]";
+        private const string ValidTurnRegex = "(?i)[a-wyz+]";
         private const char EmptySpaceChar = ' ';
 
         private static readonly Dictionary<char, (int x, int y)> Directions = new Dictionary<char, (int x, int y)>
         {
-            { 'R', (1, 0) }, { 'L', (-1, 0) }, { 'U', (0, -1) }, { 'D', (0, 1) }
+            { 'R', (1, 0) }, { 'D', (0, 1) }, { 'L', (-1, 0) }, { 'U', (0, -1) }
         };
 
         public PathAnalysisResultDto AnalyzePath(string[] map)
         {
+            var letters = new StringBuilder();
+            var path = new StringBuilder();
             var currentPosition = FindStartingPosition(map);
-            char currentChar = map[currentPosition.x][currentPosition.y];
 
-            char currentDirection = FindNextDirection(map, currentPosition);
+            path.Append(map[currentPosition.y][currentPosition.x]);
 
-            //while (!Regex.IsMatch(map[currentPosition.x][currentPosition.y].ToString(), EndingCharRegex))
-            //{
-            //    //CurrentPosition = FindNextCharacter(map, CurrentPosition);
+            char currentDirection = FindNextDirection(map, currentPosition, null);
 
-            //    //if(CurrentPosition ==)
-            //}
+            while (!Regex.IsMatch(map[currentPosition.y][currentPosition.x].ToString(), EndingCharRegex))
+            {
+                currentPosition = Move(map, currentPosition, currentDirection);
 
-            return new PathAnalysisResultDto() { Letters = "ABC", PathAsCharacters = "@---A---+|C|+---+|+-B-x" };
+                var nextChar = map[currentPosition.y][currentPosition.x];
+
+                path.Append(nextChar);
+
+                if (Regex.IsMatch(nextChar.ToString(), ValidLetterRegex))
+                {
+                    letters.Append(nextChar);
+                }
+
+                if (Regex.IsMatch(nextChar.ToString(), ValidTurnRegex))
+                {
+                    currentDirection = FindNextDirection(map, currentPosition, currentDirection);
+                }
+            }
+
+            return new PathAnalysisResultDto() { Letters = letters.ToString(), PathAsCharacters = path.ToString() };
         }
 
         private (int x, int y) FindStartingPosition(string[] map)
@@ -59,27 +77,46 @@ namespace LetterCollector.Api.Services
             return result.Value;
         }
 
-        private char FindNextDirection(string[] map, (int x, int y) position)
+        private static char FindNextDirection(string[] map, (int x, int y) position, char? currentDirection)
         {
+            // Try to continue forward if it's a letter, throw error if fake turn is detected
+            if (currentDirection != null)
+            {
+                var nextPosition = Move(map, position, currentDirection.Value);
+
+                if (IsValidPosition(map, nextPosition.x, nextPosition.y) && map[nextPosition.y][nextPosition.x] != EmptySpaceChar)
+                {
+                    if (Regex.IsMatch(map[position.y][position.x].ToString(), ValidLetterRegex))
+                    {
+                        return currentDirection.Value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid map: Fake turn detected!");
+                    }
+                }
+            }
+
             char? result = null;
+
+            // Find next turn
             foreach (var direction in Directions)
             {
-                int X = position.x + direction.Value.x;
-                int Y = position.y + direction.Value.y;
+                int x = position.x + direction.Value.x;
+                int y = position.y + direction.Value.y;
 
-                if ((Y < 0 || Y > map.Length) || (X < 0 || X > map[X].Length))
+                if (!IsValidPosition(map, x, y))
                 {
                     continue;
                 }
 
-                if (map[position.y + direction.Value.y][position.x + direction.Value.x] != EmptySpaceChar)
+                if (map[y][x] != EmptySpaceChar && direction.Key != currentDirection)
                 {
-                    if (result != null)
+                    if (currentDirection == null || (currentDirection != null && direction.Key != GetOppositeDirection(currentDirection.Value)))
                     {
-                        throw new ArgumentException("Invalid map: Cannot have 2 opposed directions!");
+                        result = direction.Key;
+                        break;
                     }
-
-                    result = direction.Key;
                 }
             }
 
@@ -88,7 +125,41 @@ namespace LetterCollector.Api.Services
                 throw new ArgumentException("Invalid map: Cannot find next direction!");
             }
 
+            var oppositePosition = Move(map, position, GetOppositeDirection(result.Value));
+
+            if (IsValidPosition(map, oppositePosition.x, oppositePosition.y) && map[oppositePosition.y][oppositePosition.x] != EmptySpaceChar)
+            {
+                throw new ArgumentException("Invalid map: Cannot have 2 opposed directions!");
+            }
+
             return result.Value;
+        }
+
+        private static char GetOppositeDirection(char direction)
+        {
+            return direction switch
+            {
+                'R' => 'L',
+                'L' => 'R',
+                'U' => 'D',
+                'D' => 'U',
+                _ => throw new ArgumentException($"Invalid direction: {direction}"),
+            };
+        }
+
+        private static (int x, int y) Move(string[] map, (int x, int y) position, char direction)
+        {
+            if (Directions.TryGetValue(direction, out var result))
+            {
+                return (x: position.x + result.x, y: position.y + result.y);
+            }
+
+            throw new ArgumentException("Invalid direction!");
+        }
+
+        private static bool IsValidPosition(string[] map, int x, int y)
+        {
+            return (y >= 0 && y < map.Length) && (x >= 0 && x < map[y].Length);
         }
     }
 }
