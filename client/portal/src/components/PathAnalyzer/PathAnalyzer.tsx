920bf7d475da3d00dc5340f2ca4da8e1a59e909d
Marko Dzimbeg <marko.dzimbeg@mediavalet.com>
2023-10-15 23:53:44 +0200

    feat: analyze path on button click

--- client/portal/src/components/PathAnalyzer/PathAnalyzer.tsx
+++ client/portal/src/components/PathAnalyzer/PathAnalyzer.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import { analyzeService } from "../../services/AnalyzerService/AnalyzerService";
 import * as Styled from "./PathAnalyzer.styles";
 
 const PathAnalyzer = () => {
+  const [mapText, setMapText] = useState("");
+
+  const analyzePath = async () => {
+    await analyzeService.analyzePath(mapText.split("\n"));
+  };
+
   return (
     <Styled.Container>
       <Styled.Title>Letter Collector</Styled.Title>
@@ -11,8 +19,8 @@ const PathAnalyzer = () => {
         <li>Collect letters</li>
         <li>Stop when you reach the character x</li>
       </Styled.RuleList>
-      <Styled.TextArea rows={6} />
-      <Styled.Button>Analyze</Styled.Button>
+      <Styled.TextArea rows={6} onChange={(e) => setMapText(e.target.value)} />
+      <Styled.Button onClick={() => analyzePath()}>Analyze</Styled.Button>
     </Styled.Container>
   );
 };
